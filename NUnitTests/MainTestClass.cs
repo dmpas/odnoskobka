4c24c165640324c23aba0185fe5b823f062f9394
Sergey Batanov <sergey.batanov@dmpas.ru>
2016-09-06 15:54:07 +0300

    Начало

---
+++ NUnitTests/MainTestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using ScriptEngine.Machine;
+using Bracker;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class MainTestClass
+    {
+
+        private EngineHelpWrapper host;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            host = new EngineHelpWrapper();
+            host.StartEngine();
+        }
+
+        [Test]
+        public void TestAsInternalObjects()
+        {
+			var w = new BrackerWriter();
+			w.SetString();
+
+			w.WriteStartElement();
+			w.WriteValue("\"N\"");
+			w.WriteValue("15");
+			w.WriteEndElement();
+
+			var result = w.Close();
+
+			Assert.AreEqual(result.DataType, DataType.String);
+			Assert.AreEqual(result.AsString(), "{\"N\",15}");
+        }
+
+        [Test]
+        public void TestAsExternalObjects()
+        {
+            host.RunTestScript("NUnitTests.Tests.external.os");
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ScriptEngine.HostedScript;
+
+namespace TestApp
+{
+	class MainClass : IHostApplication
+	{
+
+		public static HostedScriptEngine StartEngine()
+		{
+			var engine = new HostedScriptEngine();
+			engine.Initialize();
+
+			engine.AttachAssembly(System.Reflection.Assembly.GetAssembly(typeof(Bracker.BrackerWriter)));
+
+			return engine;
+		}
+
+		public static void Main(string[] args)
+		{
+
+			const string SCRIPT = @"
+	З = Новый ЗаписьСкобаря;
+	З.ЗаписатьНачалоЭлемента();
+	З.ЗаписатьЗначение(""""""#"""""");
+	З.ЗаписатьЗначение(""Array"");
+	З.ЗаписатьЗначение(2);
+
+	З.ЗаписатьНачалоЭлемента();
+	З.ЗаписатьЗначение(""""""N"""""");
+	З.ЗаписатьЗначение(15);
+	З.ЗаписатьКонецЭлемента();
+
+	З.ЗаписатьНачалоЭлемента();
+	З.ЗаписатьЗначение(""""""D"""""");
+	З.ЗаписатьЗначение(Формат(ТекущаяДата(), ""ДФ=ггггММддЧЧммсс""));
+	З.ЗаписатьКонецЭлемента();
+
+	З.ЗаписатьКонецЭлемента();
+	Сообщить(З.Закрыть());
+			";
+
+			var engine = StartEngine();
+			var script = engine.Loader.FromString(SCRIPT);
+			var process = engine.CreateProcess(new MainClass(), script);
+
+			var result = process.Start();
+
+			Console.WriteLine("Result = {0}", result);
+			Console.ReadLine();
+		}
+
+		public void Echo(string str, EchoStatus status = EchoStatus.Undefined)
+		{
+			Console.WriteLine(str);
+		}
+
+		public void ShowExceptionInfo(Exception exc)
+		{
+			Console.WriteLine(exc.ToString());
+		}
+
+		public bool InputString(out string result, int maxLen)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string[] GetCommandLineArguments()
+		{
+			return new string[] { "1", "2", "3" }; // Здесь можно зашить список аргументов командной строки
+		}
+	}
+}
